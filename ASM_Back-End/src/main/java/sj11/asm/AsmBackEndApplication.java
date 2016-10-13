9e8edafbd111a25caa0453afb405967d3905b0c6
Sergio Martin Sanchez <sergio.martinsanchez@simulation-systems.co.uk>
2016-10-13 18:03:20 +0100

    Arquitectura base. Proyecto padre con hijos front-end y back-end.

---
+++ ASM_Back-End/src/main/java/sj11/asm/AsmBackEndApplication.java
@@ -0,0 +1,12 @@
+package sj11.asm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmBackEndApplication.class, args);
+	}
+}
