8421637203fa80f2fa5ce271b3b4422c465ab1d1
SeRGiO11 <sergio.anger@gmail.com>
2017-05-28 01:56:42 +0200

    Configurada la aplicación para utilizar un servidor de correo SMTP

---
+++ ASM_Back-End/src/main/java/sj11/asm/services/EmailService.java
@@ -0,0 +1,30 @@
+package sj11.asm.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author SeRGiO11
+ */
+@Service
+public class EmailService {
+
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public void setMailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendMail(String toEmail, String subject, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(toEmail);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+        mailMessage.setFrom("info@aceitunassanchezmontes.com");
+        mailSender.send(mailMessage);
+    }
+}
