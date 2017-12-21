d45e14dc4472425928e34e53edbf8cc23b525d8b
Sergio Martin Sanchez <sergio.anger@gmail.com>
2017-12-21 13:16:58 +0000

    WIP Paso 3. Inicializacion.

--- ASM_Front-End/src/main/js/pedidos/pedidoConfirmacion.js
+++ ASM_Front-End/src/main/js/pedidos/pedidoConfirmacion.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('asm.pedidoConfirmacionController', [])
-    .controller('pedidoConfirmacionController', function ($scope, $log, $sessionStorage) {
+    .controller('pedidoConfirmacionController', function ($scope, $log, $sessionStorage, $location) {
         $log.debug("Estamos en pedidoConfirmacionController", $scope);
 
         function confirm(){
@@ -9,7 +9,25 @@ angular.module('asm.pedidoConfirmacionController', [])
             $log.debug("STEP 2", $sessionStorage.pedidoStep2);
         }
 
+        function getFormatosToDisplayFromSessionStorageStep1() {
+            var listaFormatos = [];
+            angular.forEach($sessionStorage.pedidoStep1, function(val, key, obj){
+                listaFormatos.push(obj);
+            });
+            return listaFormatos;
+        }
+
+        function initialize() {
+            if (angular.isDefined($sessionStorage.pedidoStep1 && angular.isDefined($sessionStorage.pedidoStep2))) {
+                $scope.pedido = $sessionStorage.pedidoStep2;
+                $scope.pedido.listaFormatos = getFormatosToDisplayFromSessionStorageStep1();
+            } else {
+                $location.path("/pedidos");
+            }
+        }
+
         $scope.confirm = confirm;
+        initialize();
         $scope.print = function () {
             $log.debug("$scope", $scope);
         };
